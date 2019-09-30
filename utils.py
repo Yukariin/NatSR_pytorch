b3cb569c6ce9835d377d4b67907c425910278533
Yukari <mistelinn@gmail.com>
2019-09-30 21:50:14 +0300

    Update utils

--- utils.py
+++ utils.py
@@ -1,8 +1,63 @@
+import copy
+
 import numpy as np
 import numpy.random as npr
 from scipy.fftpack import dct, idct
 import torch
+from torch import nn
 import torch.nn.functional as F
+from torchvision.transforms.functional import to_tensor
+
+
+class ImageSplitter:
+    def __init__(self, seg_size=48, scale=4, pad_size=3):
+        self.seg_size = seg_size
+        self.scale = scale
+        self.pad_size = pad_size
+        self.height = 0
+        self.width = 0
+        self.ref_pad = nn.ReplicationPad2d(self.pad_size)
+
+    def split(self, pil_img):
+        img_tensor = to_tensor(pil_img).unsqueeze(0)
+        img_tensor = self.ref_pad(img_tensor)
+        b, c, h, w = img_tensor.size()
+        self.height = h
+        self.width = w
+
+        patches = []
+        if h % self.seg_size < self.pad_size or w % self.seg_size < self.pad_size:
+            self.seg_size += self.scale * self.pad_size
+
+        for i in range(self.pad_size, h, self.seg_size):
+            for j in range(self.pad_size, w, self.seg_size):
+                patch = img_tensor[:, :,
+                    (i-self.pad_size):min(i+self.pad_size+self.seg_size, h),
+                    (j-self.pad_size):min(j+self.pad_size+self.seg_size, w)]
+
+                patches.append(patch)
+
+        return patches
+
+    def merge(self, patches):
+        pad_size = self.scale * self.pad_size
+        seg_size = self.scale * self.seg_size
+        height = self.scale * self.height
+        width = self.scale * self.width
+
+        out = torch.zeros((1, 3, height, width))
+        patch_tensors = copy.copy(patches)
+
+        for i in range(pad_size, height, seg_size):
+            for j in range(pad_size, width, seg_size):
+                patch = patch_tensors.pop(0)
+                patch = patch[:, :, pad_size:-pad_size, pad_size:-pad_size]
+
+                _, _, h, w = patch.size()
+                out[:, :, i:i+h, j:j+w] = patch
+        out = out[:, :, pad_size:-pad_size, pad_size:-pad_size]
+
+        return out
 
 
 def inject_dct(x, sigma):
@@ -25,11 +80,24 @@ def inject_dct(x, sigma):
     return Y
 
 
-def get_manifold(x, scale=4, alpha=0.5, sigma=0.1):
-    lr = F.interpolate(x, scale_factor=1/scale, mode='bicubic')
-    lr = F.interpolate(lr, scale_factor=scale, mode='bicubic')
-    a = (1-alpha)*lr + alpha*x
-
+def get_noisy(x, sigma):
     b = inject_dct(x.cpu().numpy(), sigma)
     b = torch.from_numpy(b).float().to(x.device)
-    return a, b
+    return b
+
+
+def get_blurry(x, scale, alpha):
+    lr = F.interpolate(x, scale=1/scale, mode='bicubic')
+    lr = F.interpolate(lr, scale=scale, mode='bicubic')
+    a = (1-alpha)*lr + alpha*x
+
+    return a
+
+
+def calc_acc(input, target):
+    eq = torch.eq(torch.gt(input, 0.5).float(), target)
+    return 100 * torch.mean(eq.float())
+
+
+def clamp(input, min_val, max_val):
+    return max(min(input, max_val), min_val)
